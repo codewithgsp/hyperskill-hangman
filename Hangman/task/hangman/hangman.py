bcfa2519ebdd28095db9a3e8bccda13da7334442
codewithgsp <codewithgsp@gmail.com>
2020-06-26 19:10:35 +0200

    hangman - stage 8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,4 +1,5 @@
 import random
+from string import ascii_lowercase
 
 
 class Hangman:
@@ -22,25 +23,36 @@ class Hangman:
         character_set = set(selected_word)
         self.display("\n" + mask_word)
         mask_word_list = list(mask_word)
-        turn = 0
+        typed_letter_set = set()
         while no_of_turns > 0:
             guessed_character = input('Input a letter:')
-            if guessed_character not in character_set:
-                self.display('No such letter in the word')
-                no_of_turns -= 1
-            elif guessed_character in mask_word_list:
-                self.display('No improvements')
-                no_of_turns -= 1
+            # check for single letter
+            if len(guessed_character) != 1:
+                self.display('You should input a single letter')
+            # check for lowercase ascii letter
+            elif guessed_character not in ascii_lowercase:
+                self.display('It is not an ASCII lowercase letter')
             else:
-                index = -1
-                for count in range(selected_word.count(guessed_character)):
-                    index = selected_word.find(guessed_character, index + 1)
-                    mask_word_list[index] = guessed_character
+                # condition ok to check with hidden word
+                # check for already guessed letter
+                if guessed_character in typed_letter_set:
+                    self.display('You already typed this letter')
+                else:
+                    typed_letter_set.add(guessed_character)
+                    # if new letter, compare it with the hidden word.
+                    if guessed_character not in character_set:
+                        self.display('No such letter in the word')
+                        no_of_turns -= 1
+                    else:
+                        index = -1
+                        for count in range(selected_word.count(guessed_character)):
+                            index = selected_word.find(guessed_character, index + 1)
+                            mask_word_list[index] = guessed_character
             # to display
             if no_of_turns != 0:
                 self.display("\n" + "".join(mask_word_list))
             if "".join(mask_word_list) == selected_word:
-                self.display('You guessed the word!\nYou survived!')
+                self.display('You guessed the word {}!\nYou survived!'.format(selected_word))
                 break
         else:
             self.display('You are hanged!')
@@ -51,4 +63,6 @@ class Hangman:
 
 
 hangman = Hangman(8)
-hangman.run()
+menu = input('Type "play" to play the game, "exit" to quit:')
+if menu == 'play':
+    hangman.run()
