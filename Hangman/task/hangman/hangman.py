16091c147fd1dafc6e1546375b410fd61b7be4ad
codewithgsp <codewithgsp@gmail.com>
2020-06-26 17:24:59 +0200

    hangman - stage 6

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,4 +1,3 @@
-# Write your code here
 import random
 
 
@@ -23,21 +22,32 @@ class Hangman:
         character_set = set(selected_word)
         self.display("\n" + mask_word)
         mask_word_list = list(mask_word)
-        for i in range(no_of_turns):
+        turn = 0
+        while no_of_turns > 0:
             guessed_character = input('Input a letter:')
             if guessed_character not in character_set:
                 self.display('No such letter in the word')
+                no_of_turns -= 1
+            elif guessed_character in mask_word_list:
+                self.display('No improvements')
+                no_of_turns -= 1
             else:
                 index = -1
                 for count in range(selected_word.count(guessed_character)):
                     index = selected_word.find(guessed_character, index + 1)
                     mask_word_list[index] = guessed_character
-            self.display("\n" + "".join(mask_word_list))
+            # to display
+            if no_of_turns != 0:
+                self.display("\n" + "".join(mask_word_list))
+            if "".join(mask_word_list) == selected_word:
+                self.display('You guessed the word!\nYou survived!')
+                break
+        else:
+            self.display('You are hanged!')
 
     def run(self):
         selected_word = self.select_random_word()
         self.play(self.no_of_turns, selected_word)
-        self.display("\nThanks for playing!\nWe'll see how well you did in the next stage")
 
 
 hangman = Hangman(8)
